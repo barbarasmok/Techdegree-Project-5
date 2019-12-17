c0546be6ec66ec49e7e967133540e0eada2fa685
BarbaraSmok <barb@kpimedia.com>
2019-12-17 16:12:02 -0300

    setting up the captions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // SET UP THE PHOTO GALLERY INFO
 let gallery = {
-    id: [
-    "haybales", 
+    id: ["haybales", 
     "lake", 
     "canyon", 
     "iceberg", 
@@ -12,7 +11,7 @@ let gallery = {
     "countrysidelane", 
     "sunset", 
     "cave", 
-    "bluebells"]
+    "bluebells"],
 
     caption: ["I love hay bales. Took this snap on a drive through the countryside past some straw fields.", 
     "The lake was so calm today. We had a great view of the snow on the mountains from here.", 
@@ -25,7 +24,46 @@ let gallery = {
     "We enjoyed a quiet stroll down this countryside lane.", 
     "Sunset at the coast! The sky turned a lovely shade of orange.", 
     "I did a tour of a cave today and the view of the landscape below was breathtaking.", 
-    "I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in."]
+    "I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in."],
+
+    thumbnailsImgSrc: ["./photos/thumbnails/01.jpg", 
+    "./photos/thumbnails/02.jpg", 
+    "./photos/thumbnails/03.jpg", 
+    "./photos/thumbnails/04.jpg", 
+    "./photos/thumbnails/05.jpg", 
+    "./photos/thumbnails/06.jpg", 
+    "./photos/thumbnails/07.jpg", 
+    "./photos/thumbnails/08.jpg", 
+    "./photos/thumbnails/09.jpg", 
+    "./photos/thumbnails/10.jpg", 
+    "./photos/thumbnails/11.jpg", 
+    "./photos/thumbnails/12.jpg"],
+
+    modalImgSrc: ["./photos/01.jpg", 
+    "./photos/02.jpg", 
+    "./photos/03.jpg", 
+    "./photos/04.jpg", 
+    "./photos/05.jpg", 
+    "./photos/06.jpg", 
+    "./photos/07.jpg", 
+    "./photos/08.jpg", 
+    "./photos/09.jpg", 
+    "./photos/10.jpg", 
+    "./photos/11.jpg", 
+    "./photos/12.jpg"],
+
+    imgAlt: ["Hay bales in a summer field", 
+    "Lake view with mountains in background", 
+    "Green trees and canyon area", 
+    "Iceberg and glaciers in the distance", 
+    "Desert scrubland and mesas", 
+    "A scene of the environment during the fall season", 
+    "A lush green plantation area", 
+    "Dunes at a beach facing away from the water", 
+    "A rural road surrounded by nature", 
+    "A view of the coast from on high as the sun sets", 
+    "A picture from within a cave overlooking a expanse of lush green area below", 
+    "A field of bluebells with mountains in the distance"],
 };
 
 function drawImages() {
